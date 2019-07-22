4768e4212b7aac1321fa88e459328037d5759ab1
Joachen D Busch <joachendbusch@gmail.com>
2019-07-22 16:24:47 -0700

    added tables to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,19 @@
--- TODO make schema 
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS article;
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  UserID VARCHAR (255),
+  comment VARCHAR (255),
+  FOREIGN KEY (article_id) REFERENCES article (id)
+  );
+
+CREATE TABLE article (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(255),
+  title VARCHAR(255),
+  source VARCHAR(255),
+  thumbnailurl VARCHAR (255),
+  created INT,
+  stars INT
+);
