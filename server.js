77b93e8f0dedaa545280d3e45c4e4afd678f055d
Steven Grant <blue.ninjaneer@gmail.com>
2019-07-24 12:56:11 -0700

    comments

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const dbInteractions = require('./sql.js').dbInteraction;
+
 const superagent = require('superagent')
 const port = process.env.PORT || 3000;
 const enviroment = process.env.enviroment || 'dev';
 const cookieParser = require('cookie-parser');
+
+const dbInteractions = require('./sql.js').dbInteraction;
+
 app.use(cookieParser());
 app.get('/', (req, res) => {
+  //TODO return site map w/ endpoints
   res.send('not done')
 })
 app.get('/news', (req, res) => {
@@ -26,26 +30,31 @@ app.get('/comments', (req, res) => {
 app.post('/comments', (req, res) => {
   let queryId = req.query.id;
   let message = req.query.message;
+  //If there is no message, return bad request.
   if (message.length < 1) {
     res.send(400);
   }
+  //Gets the user ID from the cookies
   let userId = req.cookies.user;
+  //If there's no id Generate one.
   if (!userId) {
     userId = Math.floor((Math.random() + 1) * 10000);
   }
+  //Passes the info to our db module.
   dbInteractions.postComments(queryId, message, userId, res)
 });
 
 //stars
 app.get('/stars', (req, res) => {
-
+  //If there's no id, return bad request, else
   if (!req.query.id) res.send(400)
   dbInteractions.getStars(req.query.id, res);
 });
 app.patch('/stars', (req, res) => {
+  //pass info to db layer
   dbInteractions.patchStar(req.query.id, res)
 });
-
+//return 404 for everything else.
 app.use('*', (req, res) => {
   res.status(404).send('nope. no. no. no.');
 })
--- sql.js
+++ sql.js
@@ -6,12 +6,14 @@ client.on('error', error => {
 })
 module.exports.dbInteraction = {
   getNews: (elements, res) => {
+    //Formatted res array of objects
     let formattedResponse = [];
-    //Wrap in promise
     elements.body.data.children.forEach(el => {
+      //Ignore lewd rude posts
       if (el.over_18) {
         return
       }
+      //This is what we get from reddit, formatted
       let formattedElement = {
         id: el.data.id,
         url: el.data.url,
@@ -21,41 +23,38 @@ module.exports.dbInteraction = {
         created: el.data.created,
         stars: 0
       };
+      //push that to the array we return.
       formattedResponse.push(formattedElement);
+      //Look up the element in the DB
       let sql =
         `SELECT * FROM article
         WHERE reddit_gen_id=$1`
       client.query(sql, [el.id]).then(sqlResponse => {
-        if (sqlResponse.rowCount > 0) {
-          formattedResponse.push(sqlResponse.rows[0].toString());
-        } else {
+        //If there's no item saved, Insert it
+        if (!(sqlResponse.rowCount > 0)) {
+          // formattedResponse.push(sqlResponse.rows[0].toString());
           sql =
             `INSERT INTO article(reddit_gen_id, url, title, source, thumbnailurl, created, stars) 
             VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *`;
           client.query(sql, [
             el.data.id, el.data.url, el.data.title, el.data.subreddit_name_prefixed, el.data.thumbnail, el.data.created, 0
-          ]
-            //       id: el.data.id,
-            // url: el.data.url,
-            // title: el.data.title,
-            // source: el.data.subreddit_name_prefixed,
-            // thumbnailurl: el.data.thumbnail,
-            // created: el.data.created,
-            // stars: 0
-          )
+          ])
         }
       }).catch(error => {
         console.error('getNews', error);
       })
     })
+    //Send Formatted Response
     res.send(formattedResponse);
   },
   getComments: (id, res) => {
+    //make sql statement to look up comments
     let sql =
       `SELECT * FROM comments
     WHERE reddit_gen_id=$1`
     client.query(sql, [id])
       .then(results => {
+        //Send results. empty array for no comments
         res.send(results.rows);
       })
       .catch(error => {
@@ -63,15 +62,18 @@ module.exports.dbInteraction = {
       })
   },
   postComments: (id, comment, userId, res) => {
+    //Generate SQL statement to insert a new comment
     let sql =
       `INSERT INTO comments(user_id, comment, reddit_gen_id) 
       VALUES ($1,$2,$3) RETURNING *`
     client.query(sql, [userId, comment, id]).then(
       sqlResponse => {
+        //Set user cookie and respond w/ the comments
         res.cookie('user', userId).send(sqlResponse.rows[0]);
       }).catch(error => { console.error('sql.js postComments', error) });
   },
   getStars: (id, res) => {
+    //Get the articles star count and return it
     let sql = `Select * FROM article WHERE reddit_gen_id=$1`
     client.query(sql, [id]).then(
       sqlRes => {
@@ -88,8 +90,7 @@ module.exports.dbInteraction = {
     })
   },
   patchStar: (id, res) => {
-    // let stars = parseInt(numberOfStars);
-    // stars++
+    //Increments a articles stars and returns it.
 
     let sql = `SELECT * FROM article 
     WHERE reddit_gen_id=$1`
